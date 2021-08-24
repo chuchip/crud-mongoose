f136a5a329a410eb65d37761b16867dcb32ffbb8
Jesús Javier Puente <jesus.javier@bosonit.com>
2021-08-24 12:47:28 +0200

    Puesto log a combined.log

--- logger.js
+++ logger.js
@@ -4,6 +4,6 @@ exports.logger= winston.createLogger({
     level: 'debug',
     transports: [
       new winston.transports.Console(),
-     // new winston.transports.File({ filename: 'combined.log' })
+      new winston.transports.File({ filename: 'combined.log' })
     ]
   });
